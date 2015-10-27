c6be96da6f2b468f87687aafbf283dd5a154dfc5
milo <milo@localhost.localdomain>
2015-10-27 20:47:42 +0000

    First major changes.
    - Created class "OrderGUI"
    	- Handles all gui code right now
    	- Contains a single button
    	- Closes elegantly
    
    - Edited main class
    	- Defined a scanner in main method
    	- Added option to launch in gui mode

--- PipesOrderSystem/src/pipesordersystem/PipesOrderSystem.java
+++ PipesOrderSystem/src/pipesordersystem/PipesOrderSystem.java
@@ -5,6 +5,8 @@
  */
 package pipesordersystem;
 
+import java.util.Scanner;
+
 /**
  *
  * @author milo
@@ -16,6 +18,26 @@ public class PipesOrderSystem {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        boolean isValid = false;
+        Scanner c = new Scanner(System.in);
+        
+        while(!isValid) {          
+            System.out.println("Start in gui mode? (y/n)");
+            String answer = c.nextLine();
+            
+            if (answer.charAt(0) == 'y') {
+                isValid = true;
+                final OrderGUI window = new OrderGUI();
+                window.setVisible(true);
+            } else if (answer.charAt(0) == 'n') {
+                isValid = true;
+                System.out.println("Starting in text mode.");
+            } else { 
+                System.out.println("\"" + answer + "\" Is not a valid input"); 
+            }
+        }
+        
     }
     
 }
